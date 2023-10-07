db2e6d178151e3e561ad2835e6d849d9dadb8dda
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2023-10-07 15:28:37 +0000

    variables

---
+++ basic/variable.js
@@ -0,0 +1,11 @@
+const accountId = 1234;
+let accountEmail = "RAjshahr@hdhd.com";
+var accountName= "RAj";
+
+
+console.log(accountEmail);
+
+/*use let instead of var 
+because of issue in block scope and function block*/
+
+console.table([accountEmail,accountId,accountName])
