e04c0a079d535a6450ec2f0e6cec3f16a26f8a25
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2023-10-08 05:26:37 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+
+console.log(3
+    +3); // code readability should be high
+
+console.log("Raj");
+
+
+let name ="Raj";
+let age = 18
+let isLoggedIn = true;
+let = state;
+
+
+// number => 2 to power
+// bigint 
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol =>unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
