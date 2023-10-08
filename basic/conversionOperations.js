fe685daa3f97ea8e1c159e111e834a9f016daa81
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2023-10-08 09:22:36 +0000

    conversionOperations

--- basic/conversionOperations.js
+++ basic/conversionOperations.js
@@ -25,5 +25,46 @@ let booleanisLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let numberString = String(someNumber);
-console.log(numberString);
-console.log(typeof numberString);
+//console.log(numberString);
+//console.log(typeof numberString);
+
+// ******************Operations**************************
+
+let value = 3;
+let negvalue = -value;
+//console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1 = "Raj"
+let str2 = " shah"
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(2+"1");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1,num2,num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
