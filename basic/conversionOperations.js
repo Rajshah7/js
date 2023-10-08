04850bf7b05046c618761daf85fb71dad008ff18
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2023-10-08 06:31:13 +0000

    conversionOperations

---
+++ basic/conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "Raj";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1,false => 0
+
+let isLoggedIn = "Raj";
+let booleanisLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanisLoggedIn);
+
+
+// 1 => true; 0 => false;
+// "" => false;
+//"Raj" => true
+
+
+let someNumber = 33;
+
+let numberString = String(someNumber);
+console.log(numberString);
+console.log(typeof numberString);
